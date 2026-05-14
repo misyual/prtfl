610d11489ac6215ba7d2838e241e43c480784e4a
misyual <clivemishall@gmail.com>
2026-05-14 16:29:36 +0800

    GhostCursor

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,32 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import './App.css'
-
+import GhostCursor from './components/GhostCursor'
 
 function App() {
   return (
     <div className="portfolio-shell">
+      <div style={{
+        position: 'fixed',
+        inset: 0,
+        width: '100vw',
+        height: '100vh',
+        zIndex: 0,
+      }}>
+        <GhostCursor
+          trailLength={50}
+          inertia={0.5}
+          grainIntensity={0.05}
+          bloomStrength={0.1}
+          bloomRadius={1}
+          brightness={2}
+          color="#B497CF"
+          edgeIntensity={0}
+        />
+      </div>
+
       <Navbar />
-      <main>
+      <main style={{ position: 'relative', zIndex: 1 }}>
         <Hero />
         <About />
         <Projects />
